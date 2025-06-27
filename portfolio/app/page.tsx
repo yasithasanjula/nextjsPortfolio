09ca52e1e4512daf95e228275a8369f804c5e954
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2025-06-27 12:02:49 +0530

    1111

---
+++ portfolio/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black">
+      <div>
+        <h1>Hellow Yasitha </h1>
+      </div>
+    </main>
+  );
+}
