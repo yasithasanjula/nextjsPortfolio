fac20c0fbff379f56c003ad252d327aaa51b7189
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2025-06-28 22:06:20 +0530

    1111

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="relative bg-black">
-      <div>
+    <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
         <h1>Hellow Yasitha </h1>
       </div>
     </main>
