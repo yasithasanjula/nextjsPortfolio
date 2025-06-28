06264567059fa924ba0296cf0dc5f88e8d2a1d44
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2025-06-28 22:50:58 +0530

    1111

--- portfolio/components/Hero.tsx
+++ portfolio/components/Hero.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import { Spotlight } from "./ui/Spotlight";
 
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
-      <div></div>
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen "
+          fill="white"
+        />
+        <Spotlight
+          className="top-10 left-full h-[80vh] w-[50vh]"
+          fill="purple"
+        />
+        <Spotlight
+          className="top-28 left-80 md:-left-32 h-[80vh] w-[50vh] "
+          fill="blue"
+        />
+      </div>
     </div>
   );
 };
